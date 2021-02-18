6b67c23d40baea79d053f3d66d13dac0e36bcd54
prior93 <Parashar.adhikary@gmail.com>
2021-02-18 18:49:41 +0200

    Initial Commit

---
+++ RealmTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmTask
+//
+//  Created by parashar.r.adhikary on 18/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
